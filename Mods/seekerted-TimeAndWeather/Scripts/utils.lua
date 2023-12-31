fb5c8c5c9b6a997dfe82e16acb93b3632165370f
Ted the Seeker <152642947+seekerted@users.noreply.github.com>
2023-12-31 21:00:19 +0800

    Upped version to 0.1.0

--- Mods/seekerted-TimeAndWeather/Scripts/utils.lua
+++ Mods/seekerted-TimeAndWeather/Scripts/utils.lua
@@ -4,7 +4,7 @@ local RegisteredHooks = {}
 
 Utils.ModName = "TimeAndWeather"
 Utils.ModAuthor = "seekerted"
-Utils.ModVer = "0.0.0"
+Utils.ModVer = "0.1.0"
 
 function Utils.Log(Format, ...)
 	print(string.format("[%s-%s] %s\n", Utils.ModAuthor, Utils.ModName, string.format(Format, ...)))
