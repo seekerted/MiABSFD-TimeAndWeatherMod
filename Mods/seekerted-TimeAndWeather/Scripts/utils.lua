7d23e2df9bd38a2753cd147b0e782fb7dc7ccbf1
Ted the Seeker <152642947+seekerted@users.noreply.github.com>
2024-01-02 18:30:57 +0800

    Changed Utils.TestFunc to display called args--To provide more information when testing functions

--- Mods/seekerted-TimeAndWeather/Scripts/utils.lua
+++ Mods/seekerted-TimeAndWeather/Scripts/utils.lua
@@ -4,7 +4,7 @@ local RegisteredHooks = {}
 
 Utils.ModName = "TimeAndWeather"
 Utils.ModAuthor = "seekerted"
-Utils.ModVer = "0.1.0"
+Utils.ModVer = "0.2.0"
 
 function Utils.Log(Format, ...)
 	print(string.format("[%s-%s] %s\n", Utils.ModAuthor, Utils.ModName, string.format(Format, ...)))
@@ -17,9 +17,15 @@ function Utils.RegisterHookOnce(FunctionName, Function)
 	end
 end
 
+-- Sends a message that the hooked function has been called, while showing the arguments.
 function Utils.TestFunc(FunctionName)
-	Utils.RegisterHookOnce(FunctionName, function()
-		Utils.Log(string.format("CALLED: %s", FunctionName))
+	Utils.RegisterHookOnce(FunctionName, function(self, ...)
+		local Args = ""
+		for _, v in ipairs({...}) do
+			Args = Args .. v:get() .. " "
+		end
+
+		Utils.Log("CALLED: %s %s", FunctionName, Args)
 	end)
 end
 
