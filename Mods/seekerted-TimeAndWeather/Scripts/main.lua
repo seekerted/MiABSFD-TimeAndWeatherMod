0d70204c3748b4fb96290182a27967ca790edf7a
Ted the Seeker <152642947+seekerted@users.noreply.github.com>
2024-01-21 19:24:05 +0800

    Removed annotations; Reworked time keeping to keep track of internal time instead, and have the game update on that

--- Mods/seekerted-TimeAndWeather/Scripts/main.lua
+++ Mods/seekerted-TimeAndWeather/Scripts/main.lua
@@ -1,6 +1,3 @@
----@alias float number
----@alias int32 integer
-
 local Utils = require("utils")
 local Config = require("config")
 
@@ -11,9 +8,8 @@ Utils.Log(_VERSION)
 local MINS_IN_HOUR = 60
 local HOURS_IN_DAY = 24
 
----On each layer, the time is {TimeSpeed} times as fast relative to the first layer. e.g. 1 second in the fifth
----layer is 6 seconds in the first layer.
----@enum (LayerNo) TimeSpeed
+-- On each layer, the time is {TimeSpeed} times as fast relative to the first layer. e.g. 1 second in the fifth
+-- layer is 6 seconds in the first layer.
 local TIME_SPEED_PER_LAYER = {
 	[1] = 1,
 	[2] = 2,
@@ -22,36 +18,45 @@ local TIME_SPEED_PER_LAYER = {
 	[5] = 6,
 }
 
----@class (exact) SaveSession
----@field PlayerTime PlayerTime
----@field PrevMapNo number?
 local SaveSession = {
-	---The Minute component is not an integer as delta values will be added to it.
-	---@class (exact) PlayerTime
-	---@field Hour integer
-	---@field Minute number
+	-- The Minute component is not an integer as delta values will be added to it.
 	PlayerTime = {
 		Hour = 0,
 		Minute = 0,
 	},
 	PrevMapNo = nil,
+	CurrentTimeSegment = nil,
+	GI = nil,
 }
 
----Gets the hour and min properties of the DateTime table and returns it as a table with {Hour, Minute}
----@param DateTime osdate
----@return table
-local function GetHourMinuteFromDateTime(DateTime)
+-- 1 Morning [4-6); 2 Daytime [6-19); 3 Evening [19-20); 4 Night [20-4)
+local function GetCorrectTimeSegment(Hour)
+	if Hour < 4 then
+		return 4
+	elseif Hour < 6 then
+		return 1
+	elseif Hour < 19 then
+		return 2
+	elseif Hour < 20 then
+		return 3
+	else
+		return 4
+	end
+end
+
+-- Gets the hour and min properties of the DateTime table and returns it as a table with {Hour, Minute}
+local function GetPlayerTimeFromOsDate(DateTime)
 	return {
 		Hour = DateTime.hour,
 		Minute = DateTime.min,
 	}
 end
 
----Adds Delta to PlayerTime (into its minutes component). Clamps the Hour and Minute values to [0-24) and [0-60) and
----adjusts accordingly when it exceeds.
----@param Delta number
----@param PlayerTime PlayerTime
+-- Adds Delta to PlayerTime (into its minutes component). Clamps the Hour and Minute values to [0-24) and [0-60) and
+-- adjusts accordingly when it exceeds.
 local function AddDeltaToPlayerTime(Delta, PlayerTime)
+	if not Delta then return end
+
 	PlayerTime.Minute = PlayerTime.Minute + Delta
 
 	if PlayerTime.Minute >= MINS_IN_HOUR then
@@ -64,10 +69,8 @@ local function AddDeltaToPlayerTime(Delta, PlayerTime)
 	end
 end
 
----Returns how long in IRL seconds the player spent on the current map
----@return number #GameStateBase.ReplicatedWorldTimeSeconds (0 if GameStateBase was invalid)
+-- Returns how long in IRL seconds the player spent on the current map
 local function GetTimeElapsedInMap()
-	---@type AGameStateBase
 	local GameStateBase = FindFirstOf("GameStateBase")
 	if not GameStateBase:IsValid() then
 		Utils.Log("Couldn't get time elapsed in map as GameStateBase was invalid")
@@ -77,10 +80,7 @@ local function GetTimeElapsedInMap()
 	return GameStateBase.ReplicatedWorldTimeSeconds
 end
 
----Returns time dilation factor, based on the depth difference of the two maps from PrevMapNo to CurrentMapNo
----@param PrevMapNo integer
----@param CurrentMapNo integer
----@return number
+-- Returns time dilation factor, based on the depth difference of the two maps from PrevMapNo to CurrentMapNo
 local function GetTimeDilation(PrevMapNo, CurrentMapNo)
 	local MIADatabaseFunctionLibrary = StaticFindObject("/Script/MadeInAbyss.Default__MIADatabaseFunctionLibrary")
 	if not MIADatabaseFunctionLibrary:IsValid() then
@@ -94,8 +94,8 @@ local function GetTimeDilation(PrevMapNo, CurrentMapNo)
 	end
 
 	-- ChangeLevel doesn't fire when changing between submaps/levels anyway, so just set the sub ID to 0.
-	local PrevMapInfo = MIADatabaseFunctionLibrary.GetMIAMapInfomation(PrevMapNo, 0)
-	local NextMapInfo = MIADatabaseFunctionLibrary.GetMIAMapInfomation(CurrentMapNo, 0)
+	local PrevMapInfo = MIADatabaseFunctionLibrary:GetMIAMapInfomation(PrevMapNo, 0)
+	local NextMapInfo = MIADatabaseFunctionLibrary:GetMIAMapInfomation(CurrentMapNo, 0)
 
 	local TimeDilation = 1
 
@@ -103,49 +103,69 @@ local function GetTimeDilation(PrevMapNo, CurrentMapNo)
 		TimeDilation = (PrevMapInfo.Depth - NextMapInfo.Depth) / 1000 + TIME_SPEED_PER_LAYER[PrevMapInfo.Floor]
 	end
 
-	Utils.Log("Time dilation from %s (MapNo: %d, Layer: %d, Depth: %dm) -> %s (MapNo: %d, Layer: %d, Depth: %dm) is %.2fx",
+	Utils.Log("Time dilation from %s (MapNo: %d, Layer: %d, Depth: %dm) -> %s (MapNo: %d, Layer: %d, Depth: %dm) is %02.2fx",
 			PrevMapInfo.Name:ToString(), PrevMapInfo.ID, PrevMapInfo.Floor, PrevMapInfo.Depth,
 			NextMapInfo.Name:ToString(), NextMapInfo.ID, NextMapInfo.Floor, NextMapInfo.Depth, TimeDilation)
 
 	return TimeDilation
 end
 
----Called when player selects a save slot to load 
----@param Param_WBP_SaveLayout_C RemoteObject
----@param Param_Index RemoteObject 0-3: Hello Abyss saves #1-4; 4-7: Deep in Abyss saves #5-8
-local function WBP_SaveLayout_C__LoadData(Param_WBP_SaveLayout_C, Param_Index)
-	SaveSession.PlayerTime = GetHourMinuteFromDateTime(os.date("*t"))
+local function ChangeGameTimeSegmentByHour(Hour)
+	SaveSession.GI:SetAbyssTime(Hour, 0)
 end
 
----Called on the fade out into darkness on change map.
----@param Param_BP_MIAGameInstance_C RemoteObject
----@param Param_MapNo RemoteObject Map number of the next map
+-- Called when player selects a save slot to load 
+-- 0-3: Hello Abyss saves #1-4; 4-7: Deep in Abyss saves #5-8
+local function WBP_SaveLayout_C__LoadData(Param_WBP_SaveLayout_C, Param_Index)
+	-- Set the PlayerTime from OS time, and set the CurrentTimeSegment given the hour.
+	SaveSession.PlayerTime = GetPlayerTimeFromOsDate(os.date("*t"))
+	SaveSession.CurrentTimeSegment = GetCorrectTimeSegment(SaveSession.PlayerTime.Hour)
+
+	Utils.Log("Loading Data (%02d:%02.2f, %d)", SaveSession.PlayerTime.Hour, SaveSession.PlayerTime.Minute, SaveSession.CurrentTimeSegment)
+end
+
+-- Called on the fade out into darkness on change map, or even any time the map is about the change (also called on Save
+-- loading)
 local function BP_MIAGameInstance_C__ChangeLevel(Param_BP_MIAGameInstance_C, Param_MapNo)
-	---@type UBP_MIAGameInstance_C
 	local BP_MIAGameInstance_C = Param_BP_MIAGameInstance_C:get()
-	if not BP_MIAGameInstance_C:IsValid() then
-		Utils.Log("BP_MIAGameInstance_C was invalid on BP_MIAGameInstance_C:ChangeLevel")
-		return
-	end
 
 	local TimeElapsedInMap = GetTimeElapsedInMap()
 	local TimeDilation = GetTimeDilation(SaveSession.PrevMapNo, BP_MIAGameInstance_C.PlayMapNo)
+
+	-- Apply time dilation delta during map change (i.e. going upwards)
+	local TimeDilationDelta = math.max(TimeElapsedInMap * (TimeDilation - 1), 0)
+	if TimeDilationDelta > 0 then
+		Utils.Log("Adding Time Dilation delta of (%02.2fs * %02.2fx =) %02.2fs", TimeElapsedInMap, TimeDilation - 1, TimeDilationDelta)
+		AddDeltaToPlayerTime(TimeDilationDelta, SaveSession.PlayerTime)
+	end
+
 	SaveSession.PrevMapNo = BP_MIAGameInstance_C.PlayMapNo
 
-	Utils.Log("Time spent: %f", TimeElapsedInMap)
+	-- Update time segment (instantly instead of transition)
+	ChangeGameTimeSegmentByHour(SaveSession.PlayerTime.Hour)
 end
 
----@param Param_BP_AbyssPlayerController_C RemoteObject 
----@param Param_DeltaSeconds RemoteObject
+-- Called on every tick of the Player Controller. Do NOT put anything too heavy in here, even Find*()
 local function BP_AbyssPlayerController_C__ReceiveTick(Param_BP_AbyssPlayerController_C, Param_DeltaSeconds)
 	AddDeltaToPlayerTime(Param_DeltaSeconds:get(), SaveSession.PlayerTime)
+
+	-- If the current time segment is not aligned with the correct time segment (based on the hour), change the time
+	-- segment to the correct one.
+	local CorrectTimeSegment = GetCorrectTimeSegment(SaveSession.PlayerTime.Hour)
+	if CorrectTimeSegment ~= SaveSession.CurrentTimeSegment then
+		Utils.Log("It is now %02d:%02.2f. Changing time segment: %d -> %d", SaveSession.PlayerTime.Hour, SaveSession.PlayerTime.Minute,
+				SaveSession.CurrentTimeSegment, CorrectTimeSegment)
+
+		SaveSession.CurrentTimeSegment = CorrectTimeSegment
+		ChangeGameTimeSegmentByHour(SaveSession.PlayerTime.Hour)
+	end
 end
 
----Hook into BP_MIAGameInstance_C instance (hot-reload friendly)
----@param New_MIAGameInstance UBP_MIAGameInstance_C
+-- Hook into BP_MIAGameInstance_C instance (hot-reload friendly)
 local function HookMIAGameInstance(New_MIAGameInstance)
 	if New_MIAGameInstance:IsValid() then
 		Utils.Log("MIAGameInstance has been found")
+		SaveSession.GI = New_MIAGameInstance
 
 		Utils.RegisterHookOnce("/Game/MadeInAbyss/UI/Save/WBP_SaveLayout.WBP_SaveLayout_C:LoadData",
 				WBP_SaveLayout_C__LoadData)
@@ -160,11 +180,15 @@ local function HookMIAGameInstance(New_MIAGameInstance)
 end
 HookMIAGameInstance(FindFirstOf("BP_MIAGameInstance_C"))
 
----@param New_MIAPlayerController AMIAPlayerController
+-- Hook into new instances of MIAPlayerController (hot-reload friendly)
 local function HookMIAPlayerController(New_MIAPlayerController)
-	-- Only use this function to hook into the player controller's ReceiveTick
-	Utils.RegisterHookOnce(
-			"/Game/MadeInAbyss/Core/GameModes/BP_AbyssPlayerController.BP_AbyssPlayerController_C:ReceiveTick",
-			BP_AbyssPlayerController_C__ReceiveTick)
+	if New_MIAPlayerController:IsValid() then
+		-- Hook into the player controller's ReceiveTick
+		Utils.RegisterHookOnce(
+				"/Game/MadeInAbyss/Core/GameModes/BP_AbyssPlayerController.BP_AbyssPlayerController_C:ReceiveTick",
+				BP_AbyssPlayerController_C__ReceiveTick)
+	else
+		NotifyOnNewObject("/Script/MadeInAbyss.MIAPlayerController", HookMIAPlayerController)
+	end
 end
-NotifyOnNewObject("/Script/MadeInAbyss.MIAPlayerController", HookMIAPlayerController)
+HookMIAPlayerController(FindFirstOf("BP_AbyssPlayerController_C"))
--- Mods/seekerted-TimeAndWeather/Scripts/utils.lua
+++ Mods/seekerted-TimeAndWeather/Scripts/utils.lua
@@ -4,18 +4,14 @@ local RegisteredHooks = {}
 
 Utils.ModName = "TimeAndWeather"
 Utils.ModAuthor = "seekerted"
-Utils.ModVer = "0.3.4"
+Utils.ModVer = "0.3.5"
 
----Prints to console in the format of [<author>-<mod>] <message> in a new line.
----@param Format string
----@param ... (string | number)?
+-- Prints to console in the format of [<author>-<mod>] <message> in a new line.
 function Utils.Log(Format, ...)
 	print(string.format("[%s-%s] %s\n", Utils.ModAuthor, Utils.ModName, string.format(Format, ...)))
 end
 
----Wrapper to UE4SS' RegisterHook except it prevents creating any more duplicate hooks according to function name.
----@param FunctionName string
----@param Function function
+-- Wrapper to UE4SS' RegisterHook except it prevents creating any more duplicate hooks according to function name.
 function Utils.RegisterHookOnce(FunctionName, Function)
 	if not RegisteredHooks[FunctionName] then
 		RegisteredHooks[FunctionName] = true
@@ -23,8 +19,7 @@ function Utils.RegisterHookOnce(FunctionName, Function)
 	end
 end
 
----Sends a message that the hooked function has been called, while showing the arguments.
----@param FunctionName string
+-- Sends a message that the hooked function has been called, while showing the arguments.
 function Utils.TestFunc(FunctionName)
 	Utils.RegisterHookOnce(FunctionName, function(self, ...)
 		local Args = ""
@@ -38,8 +33,6 @@ end
 
 -- Wraps around RegisterConsoleCommandHandler. To provide better context on which command said which log.
 -- Code inside the callback function must use Log() for logging, not Utils.Log.
----@param CommandName string Name of the command to be called in PIE. Will be prefixed with "st" to prevent collisions.
----@param Callback fun(FullCommand:string, Parameters:string, Log:fun(Message:string)):boolean If successful.
 function Utils.RegisterCommand(CommandName, Callback)
 	RegisterConsoleCommandHandler("st" .. CommandName, function(FullCommand, Parameters, OutputDevice)
 		Utils.Log("> %s", FullCommand)
