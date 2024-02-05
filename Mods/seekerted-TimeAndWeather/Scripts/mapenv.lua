3d8313e0690f9563be3e83e4e9b857f0ec8cb240
Ted the Seeker <152642947+seekerted@users.noreply.github.com>
2024-02-05 21:01:24 +0800

    Consolidate the changes made for Map 12, 13, 14, 15

--- Mods/seekerted-TimeAndWeather/Scripts/mapenv.lua
+++ Mods/seekerted-TimeAndWeather/Scripts/mapenv.lua
@@ -3,39 +3,24 @@ local Consts = require("consts")
 
 local MapEnv = {}
 
--- L2, M12 Inverted Forest
-local function OverrideMapEnv12(ME)
+-- L2, M12 Inverted Forest, M13 Hell's Crossing, M14 Inverted Arbor, M15 Seeker Camp
+-- In the inverted maps of the 2nd Layer, there's a harsh red tint on mornings, and a harsh purple tint on the distance
+-- fog in the evenings. Remove those.
+local function OverrideL2InvertedMaps(ME)
 	-- Remove the harsh red tint on L2's morning
 	ME.EnvParamsMorning.SunLightColor_10_DEFB79DF4935B10FC66149A8CCBB15C6.R = 0.226493
 	ME.EnvParamsMorning.SunLightColor_10_DEFB79DF4935B10FC66149A8CCBB15C6.B = 0.186512
-end
-
--- L2, M13 Hell's Crossing
-local function OverrideMapEnv13(ME)
-	-- Remove the harsh red tint on L2's morning
-	ME.EnvParamsMorning.SunLightColor_10_DEFB79DF4935B10FC66149A8CCBB15C6.R = 0.226493
-	ME.EnvParamsMorning.SunLightColor_10_DEFB79DF4935B10FC66149A8CCBB15C6.B = 0.186512
-end
-
--- L2, M14 Inverted Arbor
-local function OverrideMapEnv14(ME)
-	-- Remove the harsh red tint on L2's morning
-	ME.EnvParamsMorning.SunLightColor_10_DEFB79DF4935B10FC66149A8CCBB15C6.R = 0.226493
-	ME.EnvParamsMorning.SunLightColor_10_DEFB79DF4935B10FC66149A8CCBB15C6.B = 0.186512
-end
-
--- L2, M15 Seeker Camp
-local function OverrideMapEnv15(ME)
-	-- Remove the harsh red tint on L2's morning
-	ME.EnvParamsMorning.SunLightColor_10_DEFB79DF4935B10FC66149A8CCBB15C6.R = 0.226493
-	ME.EnvParamsMorning.SunLightColor_10_DEFB79DF4935B10FC66149A8CCBB15C6.B = 0.186512
+	
+	-- Remove the harsh purple tint on L2's evening
+	ME.EnvParamsEvening.BG_EmissiveColor_36_8C47B07A43219E7C2FBB4B9C402C0DFD.G = 0.276
+	ME.EnvParamsEvening.BG_EmissiveColor_36_8C47B07A43219E7C2FBB4B9C402C0DFD.R = 0.276
 end
 
 local MapEnvOverrides = {
-	[Consts.MAP_NO.INVERTED_FOREST] = OverrideMapEnv12,
-	[Consts.MAP_NO.HELLS_CROSSING] = OverrideMapEnv13,
-	[Consts.MAP_NO.INVERTED_ARBOR] = OverrideMapEnv14,
-	[Consts.MAP_NO.SEEKER_CAMP] = OverrideMapEnv15,
+	[Consts.MAP_NO.INVERTED_FOREST] = OverrideL2InvertedMaps,
+	[Consts.MAP_NO.HELLS_CROSSING] = OverrideL2InvertedMaps,
+	[Consts.MAP_NO.INVERTED_ARBOR] = OverrideL2InvertedMaps,
+	[Consts.MAP_NO.SEEKER_CAMP] = OverrideL2InvertedMaps,
 }
 
 -- Override a BP_MapEnvironment_C's variables if it exists for the specific map
