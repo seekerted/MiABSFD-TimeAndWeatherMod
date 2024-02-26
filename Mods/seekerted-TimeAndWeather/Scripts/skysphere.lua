ab11a94f8739fbb0027885b9cd9febfcff28bc34
Ted the Seeker <152642947+seekerted@users.noreply.github.com>
2024-02-26 13:52:19 +0800

    Add Sky Sphere overrides for L4 maps

--- Mods/seekerted-TimeAndWeather/Scripts/skysphere.lua
+++ Mods/seekerted-TimeAndWeather/Scripts/skysphere.lua
@@ -18,9 +18,26 @@ local function OverrideL2EdgeMaps(SS3, TimeSegment)
 	SS3:RefreshMaterial()
 end
 
+local function OverrideDeepTreeRemains(SS3, TimeSegment)
+	local Tint = {
+		{R = 0.05, G = 0.03, B = 0.02, A = 1},
+		{R = 0.1, G = 0.1, B = 0.1, A = 1},
+		{R = 0.05, G = 0.02, B = 0.01, A = 1},
+		{R = 0, G = 0, B = 0, A = 1},
+	}
+
+	SS3['Cloud color'] = {R = 1, G = 1, B = 1, A = 1}
+
+	SS3['Overall color'] = Tint[TimeSegment]
+	SS3:RefreshMaterial()
+end
+
 local SkySphere3Overrides = {
 	[Consts.MAP_NO.HEAVENS_WATERFALL] = OverrideL2EdgeMaps,
 	[Consts.MAP_NO.UPDRAFT_WASTELAND] = OverrideL2EdgeMaps,
+
+	[Consts.MAP_NO.ETERNAL_FORTUNES] = OverrideDeepTreeRemains,
+	[Consts.MAP_NO.DEEP_TREE_REMAINS] = OverrideDeepTreeRemains,
 }
 
 local function OverrideL3EdgeMaps(SS1, TimeSegment)
@@ -33,9 +50,25 @@ local function OverrideL3EdgeMaps(SS1, TimeSegment)
 	SS1:RefreshMaterial()
 end
 
+local function ManualSkySphere(SS1, TimeSegment)
+	local Tint = {
+		{R = 0.50, G = 0.48, B = 0.45, A = 1},
+		{R = 1, G = 1, B = 1, A = 1},
+		{R = 0.8, G = 0.5, B = 0.2, A = 1},
+		{R = 0, G = 0, B = 0, A = 1},
+	}
+
+	SS1['Overall color'] = Tint[TimeSegment]
+
+	SS1:RefreshMaterial()
+end
+
 local SkySphere1Overrides = {
 	[Consts.MAP_NO.THE_GREAT_FAULT] = OverrideL3EdgeMaps,
 	[Consts.MAP_NO.ROCK_SLIDE_HALL] = OverrideL3EdgeMaps,
+
+	[Consts.MAP_NO.GOBLETS_OF_GIANTS] = ManualSkySphere,
+	[Consts.MAP_NO.GIANT_VINE_BRIDGE] = ManualSkySphere,
 }
 
 -- Override a BP_Sky_Sphere*_C's variables if it exists for the specific map
